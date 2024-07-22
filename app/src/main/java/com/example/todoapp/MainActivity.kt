b9d328c2c33770e4851437a72cc11d20141a5a72
XimenaLargo96 <ximena0301@gmail.com>
2024-07-22 14:53:08 -0500

    Add layout, styles, Adapter and Holder for task's creation

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -4,17 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.todoapp.TaskCategory.*
 
 class MainActivity : ComponentActivity() {
 
     private val categories = listOf(
-        TaskCategory.Job,
-        TaskCategory.Personal,
-        TaskCategory.Other
+        Job,
+        Personal,
+        Other
     )
 
+    private val  tasks = mutableListOf(
+        Task("Prueba job", Job),
+        Task("Prueba personal", Personal),
+        Task("Prueba otro", Other)
+    )
     private lateinit var rvCategories : RecyclerView
     private lateinit var categoriesAdapter:CategoriesAdapter
+    private lateinit var rvTasks:RecyclerView
+    private lateinit var tasksAdapter: TasksAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,12 +35,17 @@ class MainActivity : ComponentActivity() {
 
     private fun initComponent() {
         rvCategories = findViewById(R.id.rvCategories)
+        rvTasks = findViewById(R.id.rvTasks)
     }
 
     private fun initUI() {
         categoriesAdapter = CategoriesAdapter(categories)
         rvCategories.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         rvCategories.adapter = categoriesAdapter
+
+        tasksAdapter = TasksAdapter(tasks)
+        rvTasks.layoutManager = LinearLayoutManager(this)
+        rvTasks.adapter = tasksAdapter
     }
 
 }
