d47ffb95e215a4f703f6b6c6aed90ff4b62ff0f4
XimenaLargo96 <ximena0301@gmail.com>
2024-07-24 14:21:18 -0500

    Add button library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.recyclerview)
     implementation(libs.androidx.cardview)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
